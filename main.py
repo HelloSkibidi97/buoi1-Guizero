d42130eb6e5dac356106a92119daf4a3885556ef
HelloSkibidi97 <phamtrinhhung6@gmail.com>
2025-09-21 22:34:18 +0700

    main.py

--- main.py
+++ main.py
@@ -3,7 +3,10 @@
 # height là chiều cao
 # bg là backgroud
 # Nêu ko để cái gì hết thì sẽ để 500 x 500
+# size là kích cỡ chữ
+# text_color là màu chữ:     noidung.text_color='color'
 from guizero import App, Text
-ungdung= App (title='Hello World',width=900,height=900,bg='white')
-noidung= Text (master=ungdung, text='Welcome to the app',bg='black')
+ungdung= App (title='Hello World',width=900,height=900,bg='silver')
+noidung= Text (master=ungdung, size=17, text='Welcome to the app',bg='white')
+noidung.text_color='gray12'
 ungdung.display()
